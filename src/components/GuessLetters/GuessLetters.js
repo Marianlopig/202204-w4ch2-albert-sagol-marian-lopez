ed9f7272353718fc2d0d591533b4727896e85fa3
Marian <marianlopig@gmail.com>
2022-04-26 23:41:08 +0200

    show selected letters

--- src/components/GuessLetters/GuessLetters.js
+++ src/components/GuessLetters/GuessLetters.js
@@ -3,12 +3,12 @@ import "./GuessLetters.css";
 const GuessLetters = ({ word, chossenLetters }) => {
   return (
     <ul className="guess-letters">
-      {word.split("").map((letter) => (
-        <li className="guess-letter empty">-</li>
-      ))}
-
-      {/* <li className="guess-letter empty"></li>
-      <li className="guess-letter">A</li> */}
+      {word.split("").map((letter) => {
+        if (chossenLetters.includes(letter.toUpperCase())) {
+          return <li className="guess-letter">{letter.toUpperCase}</li>;
+        }
+        return <li className="guess-letter empty">*</li>;
+      })}
     </ul>
   );
 };
