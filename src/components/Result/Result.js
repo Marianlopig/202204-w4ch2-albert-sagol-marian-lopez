61abbcfca4de77f958a77ca35ac6d1ca0518705c
Marian <marianlopig@gmail.com>
2022-04-26 19:49:38 +0200

    result component changed

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,7 +1,10 @@
 import "./Result.css";
+import PropTypes from "prop-types";
 
-const Result = () => {
-  return <section className="game-result">You're dead!</section>;
+const Result = (text) => {
+  return <section className="game-result">{text}</section>;
 };
+
+Result.propTypes = { text: PropTypes.string.isRequired };
 
 export default Result;
