1a8bf089711f62ca531dfee5dbddf4f59549f08d
Marian <marianlopig@gmail.com>
2022-04-26 20:41:04 +0200

    array letters and function to take the letter

--- src/components/Letters/Letters.js
+++ src/components/Letters/Letters.js
@@ -1,91 +1,21 @@
 import "./Letters.css";
+import PropTypes from "prop-types";
 
-const Letters = () => {
+const Letters = ({ alphabet, action }) => {
   return (
     <ul className="letters">
-      <li className="letter">
-        <a href="a">A</a>
-      </li>
-      <li className="letter">
-        <a href="b">B</a>
-      </li>
-      <li className="letter">
-        <a href="c">C</a>
-      </li>
-      <li className="letter">
-        <a href="d">D</a>
-      </li>
-      <li className="letter">
-        <a href="e">E</a>
-      </li>
-      <li className="letter">
-        <a href="f">F</a>
-      </li>
-      <li className="letter">
-        <a href="g">G</a>
-      </li>
-      <li className="letter">
-        <a href="h">H</a>
-      </li>
-      <li className="letter">
-        <a href="i">I</a>
-      </li>
-      <li className="letter">
-        <a href="j">J</a>
-      </li>
-      <li className="letter">
-        <a href="k">K</a>
-      </li>
-      <li className="letter">
-        <a href="l">L</a>
-      </li>
-      <li className="letter">
-        <a href="m">M</a>
-      </li>
-      <li className="letter">
-        <a href="n">N</a>
-      </li>
-      <li className="letter">
-        <a href="ñ">Ñ</a>
-      </li>
-      <li className="letter">
-        <a href="o">O</a>
-      </li>
-      <li className="letter">
-        <a href="p">P</a>
-      </li>
-      <li className="letter">
-        <a href="q">Q</a>
-      </li>
-      <li className="letter">
-        <a href="r">R</a>
-      </li>
-      <li className="letter">
-        <a href="s">S</a>
-      </li>
-      <li className="letter">
-        <a href="t">T</a>
-      </li>
-      <li className="letter">
-        <a href="u">U</a>
-      </li>
-      <li className="letter">
-        <a href="v">V</a>
-      </li>
-      <li className="letter">
-        <a href="w">W</a>
-      </li>
-      <li className="letter">
-        <a href="x">X</a>
-      </li>
-      <li className="letter">
-        <a href="y">Y</a>
-      </li>
-      <li className="letter">
-        <a href="z">Z</a>
-      </li>
+      {alphabet.map((letter) => (
+        <li key={letter} className="letter" onClick={action}>
+          <a href={letter}>{letter.toUpperCase()}</a>
+        </li>
+      ))}
     </ul>
   );
+};
+
+Letters.propTypes = {
+  alphabet: PropTypes.arrayOf(PropTypes.string).isRequired,
+  action: PropTypes.func.isRequired,
 };
 
 export default Letters;
